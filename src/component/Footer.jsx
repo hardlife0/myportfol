69da1308f68749309bab3fc5f0dfb503297a665c
hardlife0 <hellosy76@naver.com>
2024-04-27 12:47:31 +0900

    틀 완성

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <footer id="footer" role="contentinfo">
+            <div className="footer__inner">
+                <div className="footer__info">
+                    <div className="right">
+                        <h3>social</h3>
+                        <ul>
+                            <li>
+                                <a href="https://www.youtube.com/@Webstoryboy">010-6823-7088</a>
+                                <em>전화주시면 조금 더 빠른 상담이 가능합니다.</em>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
